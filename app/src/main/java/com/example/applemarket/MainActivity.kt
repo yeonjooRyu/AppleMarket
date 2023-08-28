7472606ec66bd81726e2ace35fd7553202d07a1b
yeonjooRyu <yeonjoo3751@gmail.com>
2023-08-28 20:47:18 +0900

    [feature] 아이콘 및 이미지 추가

--- app/src/main/java/com/example/applemarket/MainActivity.kt
+++ app/src/main/java/com/example/applemarket/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.applemarket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// 커밋 확인 테스트
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
